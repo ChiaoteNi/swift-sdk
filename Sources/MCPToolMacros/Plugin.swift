bd059e0bb54103dc5be8604df3c66ed4eec4f022
Chiaote Ni <aaron.chiao.19@gmail.com>
2025-08-05 19:15:29 +0800

    chore: remove unused implementations

--- Sources/MCPToolMacros/Plugin.swift
+++ Sources/MCPToolMacros/Plugin.swift
@@ -4,9 +4,6 @@ import SwiftSyntaxMacros
 @main
 struct MCPToolMacrosPlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-        SchemaFieldMacro.self,
-        InputSchemaMacro.self,
-        OutputSchemaMacro.self,
         SchemaMacro.self,
         FieldMacro.self,
     ]
